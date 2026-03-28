2eb142c06095ad478b9c09da0cc7e4b90a4f5358
Danesh-ice <sodadanesh@gmail.com>
2026-03-28 20:19:34 +0800

    UI
    I have doen the UI there were some bugs with some of the asset adding therefore I did not add those in
    first was the asset background for the top right corner quiz
    and  another one was adding the current quiz at the pause menu other then that it work from my testing

---
+++ main_menu.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+@onready var setting: Panel = $Setting
+@onready var volume: HSlider = $Setting/Volume
+@onready var master_bus = AudioServer.get_bus_index("Master")
+@onready var how_to_play_ui: Panel = $"How to play UI"
+
+func _ready():
+	setting.visible = false
+	how_to_play_ui.visible = false
+	volume.value = db_to_linear(AudioServer.get_bus_volume_db(master_bus))
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://newMain.tscn")
+
+func _on_settings_pressed() -> void:
+	print("settigs pressed")
+	setting.visible = true
+
+func _on_how_to_play_pressed() -> void:
+	how_to_play_ui.visible = true
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_back_pressed() -> void:
+	_ready()
+
+
+func _on_volume_value_changed(new_value: float) -> void:
+	AudioServer.set_bus_volume_db(master_bus, linear_to_db(new_value))
+	AudioServer.set_bus_mute(master_bus, new_value < 0.01)
